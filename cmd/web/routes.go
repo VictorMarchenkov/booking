7cb397a2011f5c1e11a539cb67794e1de64b7a39
Victor <victormv03@gmail.com>
2021-03-01 22:16:26 +0300

    Initial commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/VictorMarchenkov/booking/pkg/config"
+	"github.com/VictorMarchenkov/booking/pkg/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"net/http"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	//mux := pat.New()
+	//mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	//mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	return mux
+}
